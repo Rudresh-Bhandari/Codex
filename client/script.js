97152575740704c1b114481c035b25d8cc70d116
Rudresh <therudreshmister@gmail.com>
2022-12-27 10:46:15 -0700

    made changes to script js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codexxxx-8lr1.onrender.com ', {
+    const response = await fetch('https://codex-oy2s.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
